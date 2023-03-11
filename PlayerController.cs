34aebce7de129b7775799a1a16379d5dc4ba9a9b
AlejandroQuintaFarina <91197989+AlejandroQuintaFarina@users.noreply.github.com>
2023-03-11 16:16:50 +0100

    RollaBall

---
+++ PlayerController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float moveSpeed = 10f;
+    private float xInput;
+    private float yInput;
+    
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ProcessInputs()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+    }
+
+    private void Move()
+    {
+        rb.AddForce(new Vector3(xInput, 0f, zInput));
+    }
+}
